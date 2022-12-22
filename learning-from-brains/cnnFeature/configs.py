c87da1e041e50969bf14a01aa29b8bed3ff0d81d
ghaith2812@outlook.com <ghaith2812@outlook.com>
2022-12-22 15:52:52 -0500

    relative path2

--- learning-from-brains/cnnFeature/configs.py
+++ learning-from-brains/cnnFeature/configs.py
@@ -1,10 +1,13 @@
 import torch
 import os
+from dirname import basePath
 #dataPath="C:/Users/ghait/Repos/SSFLData/train"
-dataPath="C:/Users/ghait/Repos/learningFromBrains/hcp/100307/analysis"
+dataPath=basePath+"/hcp/100307/analysis"
+#dataPath="C:/Users/ghait/Repos/learningFromBrains/hcp/100307/analysis"
 #validationDataPath="C:/Users/ghait/Repos/SSFLData/val"
 validationDataPath=None
-resultPath="C:/Users/ghait/Repos/learningFromBrains/learning-from-brains/cnnFeature/results"
+#resultPath="C:/Users/ghait/Repos/learningFromBrains/learning-from-brains/cnnFeature/results"
+resultPath=basePath+"/cnnFeature/results"
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 learning_rate=0.0005
 num_epochs=1500
