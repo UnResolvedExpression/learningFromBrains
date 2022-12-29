d8761e6297f3178698942d69efa73c7b429922d5
ghaith2812@outlook.com <ghaith2812@outlook.com>
2022-12-29 10:10:27 -0500

    adjusted glob5

--- learning-from-brains/src/parallelConncectomeLoader.py
+++ learning-from-brains/src/parallelConncectomeLoader.py
@@ -82,7 +82,7 @@ def loadConnectome(sub,task,run,sample) -> Dict[str, torch.tensor]:
     #     directoryPath = BasePath + "/hcp/{}/analysis/{}_2_fsaverage_tfMRI_{}_LR-lh.stc".format(sub, sub, task)
     #     # print(directoryPath)
     #     # print(os.path.isfile(directoryPath))
-    fmriData=mne.read_source_estimate(directoryPath)
+    fmriData=mne.read_source_estimate(directoryPath[0])
 
 
     # fileObj=open(directoryPath)
