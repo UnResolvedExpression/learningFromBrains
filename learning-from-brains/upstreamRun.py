8dcada4b866e34e49c111b717f79c51479bdd274
ghaith2812@outlook.com <ghaith2812@outlook.com>
2023-01-04 13:41:51 -0500

    lets try one step to see if the validation works

--- learning-from-brains/upstreamRun.py
+++ learning-from-brains/upstreamRun.py
@@ -1,4 +1,4 @@
 import os
 
 if __name__ == '__main__':
-    os.system("python scripts/train.py --data data/upstream/HCP --architecture GPT --training-style CSM --training-steps 1000 --validation-steps 100 --test-steps 10 --log-every-n-steps 100 --per-device-training-batch-size 256 --per-device-validation-batch-size 256 --learning-rate 1e-4 --log-dir results/models/upstream/HCP --log-every-n-steps 100")
+    os.system("python scripts/train.py --data data/upstream/HCP --architecture GPT --training-style CSM --training-steps 1000 --validation-steps 100 --test-steps 10 --log-every-n-steps 100 --per-device-training-batch-size 256 --per-device-validation-batch-size 256 --learning-rate 1e-4 --log-dir results/models/upstream/HCP")
