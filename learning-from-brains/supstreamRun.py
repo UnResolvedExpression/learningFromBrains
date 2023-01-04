cccdf29a21be6edc475de8e92d973ad6bbffc686
ghaith2812@outlook.com <ghaith2812@outlook.com>
2023-01-04 13:41:16 -0500

    clone detach to increase speed

--- learning-from-brains/supstreamRun.py
+++ learning-from-brains/supstreamRun.py
@@ -1,4 +1,4 @@
 import os
 
 if __name__ == '__main__':
-    os.system("python scripts/train.py --data data/upstream/HCP --n-train-subjects-per-dataset 6 --n-val-subjects-per-dataset 2 --n-test-subjects-per-dataset 5 --architecture GPT --training-style CSM --training-steps 10000 --per-device-training-batch-size 64 --learning-rate 1e-4 --log-dir results/models/upstream/HCP --log-every-n-steps 1000")
+    os.system("python scripts/train.py --data data/upstream/HCP --architecture GPT --training-style CSM --training-steps 1 --validation-steps 1 --test-steps 1 --per-device-training-batch-size 256 --per-device-validation-batch-size 256 --learning-rate 1e-4 --log-dir results/models/upstream/HCP --log-every-n-steps 1")
