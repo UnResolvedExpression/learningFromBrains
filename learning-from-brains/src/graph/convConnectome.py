58e738a0fabd9063a66dc84f2025ce8cb7fa732b
ghaith2812@outlook.com <ghaith2812@outlook.com>
2023-01-04 11:08:32 -0500

    debug fmri slicing4

--- learning-from-brains/src/graph/convConnectome.py
+++ learning-from-brains/src/graph/convConnectome.py
@@ -76,6 +76,8 @@ class Connectome():
         print('sample issue')
         print(fmriData.shape)
         print(int(0))
+        print(fmriData)
+        print(fmriData[0])
         fmriData=fmriData[ 0:fmriData.shape[0] , 0 ]
         print(fmriData.shape)
 
