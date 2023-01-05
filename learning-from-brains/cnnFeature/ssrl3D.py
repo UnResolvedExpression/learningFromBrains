b1c4501850aac9221ec42bdffc2196bb3d6c7742
ghaith2812@outlook.com <ghaith2812@outlook.com>
2023-01-04 22:27:20 -0500

    lets see if we can get a 3D encoder17

--- learning-from-brains/cnnFeature/ssrl3D.py
+++ learning-from-brains/cnnFeature/ssrl3D.py
@@ -72,7 +72,7 @@ if __name__ == '__main__':
                 print(data.shape)
                 epoch_loss=0
                 data=torch.squeeze(data)
-                data = data.astype(np.float)
+                data = np.array(data).astype(np.float)
                 for timePoint in range(0,data.shape[3]):
                     tepoch.set_description(f"Epoch {epoch} Timepoint {timePoint}")
                     preimg = torch.nn.functional.normalize(data,p=2.0,dim=3) #this is potentially a dataleak...
