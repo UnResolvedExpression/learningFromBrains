f2db075d8e2f5f5b0a3ae3cf30a50e1a651f2ce9
ghaith2812@outlook.com <ghaith2812@outlook.com>
2023-01-04 23:16:18 -0500

    lets see if we can get a 3D encoder 24

--- learning-from-brains/cnnFeature/secondCnnModel3D.py
+++ learning-from-brains/cnnFeature/secondCnnModel3D.py
@@ -25,11 +25,11 @@ class Dec(nn.Module):
             # nn.ConvTranspose3d(8, 4, 2, stride=1, output_padding=0),#1
             # nn.ConvTranspose3d(4, 1, 2, stride=1, output_padding=0)
 
-            nn.ConvTranspose2d(8, 16, 3, stride=2),  # b, 16, 5, 5
+            nn.ConvTranspose3d(8, 16, 3, stride=2),  # b, 16, 5, 5
             nn.ReLU(True),
-            nn.ConvTranspose2d(16, 8, 5, stride=3, padding=1),  # b, 8, 15, 15
+            nn.ConvTranspose3d(16, 8, 5, stride=3, padding=1),  # b, 8, 15, 15
             nn.ReLU(True),
-            nn.ConvTranspose2d(8, 1, 2, stride=2, padding=1),  # b, 1, 28, 28
+            nn.ConvTranspose3d(8, 1, 2, stride=2, padding=1),  # b, 1, 28, 28
         )
 
     def forward(self, x):
