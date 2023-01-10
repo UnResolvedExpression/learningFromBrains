9ec00c8a09bf942bb25ef12855d8c2e21d88315a
ghaith2812@outlook.com <ghaith2812@outlook.com>
2023-01-09 19:47:03 -0500

    lets try running ssrl 3D again

--- learning-from-brains/cnnFeature/ssrl.py
+++ learning-from-brains/cnnFeature/ssrl.py
@@ -111,7 +111,7 @@ if __name__ == '__main__':
             for var_name in decOptimizer.state_dict():
                 print(var_name, "\t", decOptimizer.state_dict()[var_name])
 
-            description="3D_version"
+            description="1D_version_tiny"
 
             torch.save({'epoch': epoch,
             'model_state_dict': enc.state_dict(),
--- learning-from-brains/cnnFeature/ssrl3D.py
+++ learning-from-brains/cnnFeature/ssrl3D.py
@@ -161,7 +161,7 @@ if __name__ == '__main__':
             for var_name in decOptimizer.state_dict():
                 print(var_name, "\t", decOptimizer.state_dict()[var_name])
 
-            description = "3D_version_tiny_ample"
+            description = "3D_version"
 
             torch.save({'epoch': epoch,
                         'model_state_dict': enc.state_dict(),
